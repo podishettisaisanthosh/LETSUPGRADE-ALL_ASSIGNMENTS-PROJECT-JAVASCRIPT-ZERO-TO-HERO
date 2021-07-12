9ac8703220b8626d4383e4ee30fa8cf3b80f9e76
podishettisaisanthosh <87226601+podishettisaisanthosh@users.noreply.github.com>
2021-07-12 19:40:55 +0530

    LETSUPGRADE/JS/ASSIGNMENT:2/Q1/FIZZBUZZ
    PODISHETTI SAI SANTHOSH SUBMITTED

---
+++ ASSIGNMENT:2/index.js
@@ -0,0 +1,29 @@
+  
+//program for fizzbuzz
+// using Number.Integer() method
+//BY PODAISHETTI SAISANTHOSH 
+//LETSUPGRADE JAVASCRIPT ZERO TO HERO
+
+
+
+for(var i=1;i<=100;i++)
+{
+  if(Number.isInteger(i/15)){
+    console.log("FizzBuzz") 
+    
+  } 
+    else if(Number.isInteger(i/3)){
+      console.log("Fizz") 
+      
+    } 
+    else if(Number.isInteger(i/5)){ 
+      console.log("Buzz")
+      }
+      else{ 
+        console.log(i)
+        }
+}
+
+
+
+
